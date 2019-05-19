23775924d9444116f77249942ab9c6ef5ae48d84
maseko <ekoaewes2006@gmail.com>
2019-05-20 00:04:35 +0700

    add constructure auth

--- app/Http/Controllers/AccpengajuanController.php
+++ app/Http/Controllers/AccpengajuanController.php
@@ -12,6 +12,10 @@ class AccpengajuanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         return view('accpengajuan.index');
